5b9181fe87771232ab36cff84c46184cf3df7527
JQrdan <solfonictoast@gmail.com>
2020-01-22 14:41:49 +0000

    feat: create elastic sink connector config
    Signed-off-by: JQrdan <solfonictoast@gmail.com>

--- scripts/createConnectTopics.sh
+++ scripts/createConnectTopics.sh
@@ -2,11 +2,11 @@
 
 readonly ZOOKEEPER=zookeeper:2181
 
-echo "Creating connect-configs topic"
+echo "Creating topic connect-configs"
 ../bin/kafka-topics.sh --create --zookeeper ZOOKEEPER --replication-factor 3 --partitions 1 --topic connect-configs
 
-echo "Creating connect-offsets topic"
+echo "Creating topic connect-offsets"
 ../bin/kafka-topics.sh --create --zookeeper ZOOKEEPER --replication-factor 3 --partitions 25 --topic connect-offsets
 
-echo "Creating connect-status topic"
+echo "Creating topic connect-status topic"
 ../bin/kafka-topics.sh --create --zookeeper ZOOKEEPER --replication-factor 3 --partitions 5 --topic connect-status
